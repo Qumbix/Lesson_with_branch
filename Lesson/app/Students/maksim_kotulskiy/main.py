7a66b035d2b1ddf5defa434cbd2a11ed934b5603
Qumbix <kotu.maks@gmail.com>
2024-01-13 16:21:56 +0200

    added multiplication function

--- Lesson/app/Students/maksim_kotulskiy/main.py
+++ Lesson/app/Students/maksim_kotulskiy/main.py
@@ -1,6 +1,1 @@
-#if __name__ == "__main__":
-    #x1 = int(input("Перше число: "))
-    #x2 = int(input("Друге число: "))
-def multiplication(x1, x2):
-    result = x1 * x2
-    return result
+print("Hello, World!")
