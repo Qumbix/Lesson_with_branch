f91e353b9eb2891962015115978283fc3c7f5160
Qumbix <kotu.maks@gmail.com>
2024-01-13 16:15:31 +0200

    added multiplication function

--- Lesson/app/views.py
+++ Lesson/app/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+import Students.maksim_kotulskiy.main as func
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
     return render(request, "index.html")
+
+x1 = int(input("Перше число: "))
+x2 = int(input("Друге число: "))
+print(func.multiplication(x1, x2))
